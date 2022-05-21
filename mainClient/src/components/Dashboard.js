f7fa3cf3df847486352f50b576e1c0b6082388dc
Zach <zgleason37@gmail.com>
2022-05-20 19:47:51 -0700

    Added Programs & Recipes

--- mainClient/src/components/Dashboard.js
+++ mainClient/src/components/Dashboard.js
@@ -18,7 +18,21 @@ import AccessTimeIcon from '@mui/icons-material/AccessTime';
 import LocalDiningIcon from '@mui/icons-material/LocalDining';
 import ArrowCircleRightIcon from '@mui/icons-material/ArrowCircleRight';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import pic from '../Assets/bike.jpg';
+import pic2 from '../Assets/dumbell.jpeg'
+import pic from '../Assets/lift.jpeg'
+import pic3 from '../Assets/rope.jpeg'
+import pic4 from '../Assets/group.jpeg'
+import pic5 from '../Assets/squats.jpeg'
+import pic6 from '../Assets/cycle.jpeg'
+// import Pagination from '@mui/material/Pagination';
+// import Stack from '@mui/material/Stack';
+import HorizontalScroll from 'react-scroll-horizontal'
+import food1 from '../Assets/food.jpg'
+import food2 from '../Assets/salad.jpg'
+import food3 from '../Assets/pasta.jpg'
+import food4 from '../Assets/chicken.jpg'
+import food5 from '../Assets/avacado.jpg'
+
 
 
 
@@ -39,6 +53,7 @@ import { Pie } from 'react-chartjs-2';
 
 
 const Dashboard = () => {
+
 
     ChartJS.register(
         CategoryScale,
@@ -173,19 +188,66 @@ const Dashboard = () => {
                     <Button sx={{ color: "text.primary", borderRadius: "30px", ml: -1, mt: -0.5}}><AddBoxIcon /></Button>
                 </Typography>
             </Box>
-            <Box sx={{ border: "1px solid black", height: "420px", width: "720px", borderRadius: "20px", mt: -53, ml: 91, overflowX: "scroll"}} id="box">
-            <Typography sx={{ position: "absolute", mt: -5, ml: 1, fontSize: 25, fontFamily: "serif" }}>
-                    Programs / Categories
-                </Typography>
-                <Box sx={{ border: "1px solid black", height: "190px", width: "226px", borderRadius: "30px", mt: 1, ml: 1, display: "inline-block" }}>
-                    <img src={pic} alt="test" width={210} height={200} borderRadius={50}/>
+                <Box sx={{ border: "1px solid black", height: "420px", width: "720px", borderRadius: "20px", mt: -53, ml: 91, overflowX: "scroll"}} id="box">
+                        {/* <Stack spacing={2} sx={{ ml: -100, mt: -5}}>
+                            <Pagination count={2} />
+                        </Stack> */}
+                <Typography sx={{ position: "absolute", mt: -5, ml: 1, fontSize: 25, fontFamily: "serif" }}>
+                        Programs / Categories
+                    </Typography>
+                <HorizontalScroll>
+                    <Box sx={{ border: "1px solid black", height: "400px", width: "226px", borderRadius: "20px", mt: 1, ml: 1.,  }}>
+                        <img src={pic2} alt="test" width={"226px"} height={"400px"} className="program"  style={{borderRadius: '20px'}}/>
+                        <Typography sx={{ color: "White", fontSize: 23, top: 0, mt: -49, ml: 2.5, fontFamily: "Koulen", letterSpacing: "0.02px", }}>
+                                THE 10 MINUTE, MINIMAL-SPACE FULL-BODY WORKOUT
+                        </Typography>
+                        <Box sx={{ width: "215px", height: "40px", background: "black", position: "absolute", borderRadius: "20px", mt: 29.5, ml: 0.8, opacity: 0.7}}>
+                            <Typography sx={{ color: "white", fontSize: 23, top: 0, mt: 0.5, ml: 1, fontFamily: "Koulen", position: "absolute" }}>
+                                    Intensity
+                            </Typography>
+                        </Box>
+                        <Box sx={{ height: "10px", width: "25px", borderRadius: "40px", top: 0, position: "absolute", mt: 46.5, ml: 13.5, background: "linear-gradient(#66ff33, #ccff66);"}} ></Box>
+                        {/* <Box sx={{ height: "10px", width: "25px", borderRadius: "40px", top: 0, position: "absolute", mt: 47, ml: 17.5, background: "linear-gradient(#ffff66, #ff9933);"}} ></Box> */}
+                    </Box>
+                    <Box sx={{ border: "1px solid black", height: "400px", width: "226px", borderRadius: "30px", mt: 1, ml: 1.5, }}>
+                        <img src={pic} alt="test" width={"226px"} height={"400px"} className="program" overflow={'hidden'} style={{borderRadius: '30px'}}/>
+                        <Typography sx={{ color: "White", fontSize: 23, top: 0, mt: -49, ml: 2.5, fontFamily: "Koulen", letterSpacing: "0.02px" }}>
+                                THE ULTIMATE UPPER-BODY WORKOUT ROUTINE
+                        </Typography>
+                        <Box sx={{ width: "215px", height: "40px", background: "black", position: "absolute", borderRadius: "20px", mt: 34, ml: 0.8, opacity: 0.7}}>
+                            <Typography sx={{ color: "white", fontSize: 23, top: 0, mt: 0.5, ml: 1, fontFamily: "Koulen", position: "absolute" }}>
+                                    Intensity
+                            </Typography>
+                        </Box>
+                        <Box sx={{ height: "10px", width: "25px", borderRadius: "40px", top: 0, position: "absolute", mt: 47, ml: 13.5, background: "linear-gradient(#66ff33, #ccff66);"}} ></Box>
+                        <Box sx={{ height: "10px", width: "25px", borderRadius: "40px", top: 0, position: "absolute", mt: 47, ml: 17.5, background: "linear-gradient(#ffff66, #ff9933);"}} ></Box>
+                        <Box sx={{ height: "10px", width: "25px", borderRadius: "40px", top: 0, position: "absolute", mt: 47, ml: 21.5, background: "linear-gradient(#ff6600, #ff0000);"}} ></Box>
+                    </Box>
+                    <Box sx={{ border: "1px solid black", height: "400px", width: "226px", borderRadius: "30px", mt: 1, ml: 1.5,}}>
+                        <img src={pic3} alt="test" width={"226px"} height={"400px"} className="program" overflow={'hidden'} style={{borderRadius: '30px'}}/>
+                        <Typography sx={{ color: "White", fontSize: 23, top: 0, mt: -49, ml: 2.5, fontFamily: "Koulen", letterSpacing: "0.02px" }}>
+                                FUNCTIONAL FITNESS WORKOUT PROGRAM
+                        </Typography>
+                        <Box sx={{ width: "215px", height: "40px", background: "black", position: "absolute", borderRadius: "20px", mt: 34, ml: 0.8, opacity: 0.7}}>
+                            <Typography sx={{ color: "white", fontSize: 23, top: 0, mt: 0.5, ml: 1, fontFamily: "Koulen", position: "absolute" }}>
+                                    Intensity
+                            </Typography>
+                        </Box>
+                        <Box sx={{ height: "10px", width: "25px", borderRadius: "40px", top: 0, position: "absolute", mt: 47, ml: 13.5, background: "linear-gradient(#66ff33, #ccff66);"}} ></Box>
+                        <Box sx={{ height: "10px", width: "25px", borderRadius: "40px", top: 0, position: "absolute", mt: 47, ml: 17.5, background: "linear-gradient(#ffff66, #ff9933);"}} ></Box>
+                        {/* <Box sx={{ height: "10px", width: "25px", borderRadius: "40px", top: 0, position: "absolute", mt: 47, ml: 21.5, background: "linear-gradient(#ff6600, #ff0000);"}} ></Box> */}
+                    </Box>
+                    <Box sx={{ border: "1px solid black", height: "400px", width: "226px", borderRadius: "30px", mt: 1, ml: 1.5, }}>
+                        <img src={pic4} alt="test" width={"226px"} height={"400px"} className="program" overflow={'hidden'} style={{borderRadius: '30px'}}/>
+                    </Box>
+                    <Box sx={{ border: "1px solid black", height: "400px", width: "226px", borderRadius: "30px", mt: 1, ml: 1.5, }}>
+                        <img src={pic5} alt="test" width={"226px"} height={"400px"} className="program" overflow={'hidden'} style={{borderRadius: '30px'}}/>
+                    </Box>
+                    <Box sx={{ border: "1px solid black", height: "400px", width: "226px", borderRadius: "30px", mt: 1, ml: 1.5, }}>
+                        <img src={pic6} alt="test" width={"226px"} height={"400px"} className="program" overflow={'hidden'} style={{borderRadius: '30px'}}/>
+                    </Box>
+                </HorizontalScroll>
                 </Box>
-                <Box sx={{ border: "1px solid black", height: "190px", width: "226px", borderRadius: "30px", mt: 1, ml: 1.5, display: "inline-block"}}></Box>
-                <Box sx={{ border: "1px solid black", height: "190px", width: "226px", borderRadius: "30px", mt: 1, ml: 1.5, display: "inline-block"}}></Box>
-                <Box sx={{ border: "1px solid black", height: "190px", width: "226px", borderRadius: "30px", mt: 1, ml: 1.5, display: "inline-block"}}></Box>
-                <Box sx={{ border: "1px solid black", height: "190px", width: "226px", borderRadius: "30px", mt: 1, ml: 1.5, display: "inline-block"}}></Box>
-                <Box sx={{ border: "1px solid black", height: "190px", width: "226px", borderRadius: "30px", mt: 1, ml: 1.5, display: "inline-block"}}></Box>
-            </Box>
             <Box sx={{ border: "1px solid black", height: "420px", width: "300px", borderRadius: "20px", mt: 6, ml: 144}}>
                 <Typography sx={{ position: "absolute", mt: -5, ml: 1, fontSize: 25, fontFamily: "serif" }}>
                     Drink Log
@@ -200,12 +262,21 @@ const Dashboard = () => {
                     Recipes
                 </Typography>
                 <Box sx={{ width: "582px", height: "95px", border: "2px solid black", borderRadius: "10px", ml: 1, mt: 1,  }} >
-                <FontAwesomeIcon icon="fa-solid fa-bowl-food" />
-                    <ArrowCircleRightIcon sx={{ ml: 67, mt: 3, fontSize: 38, color: "text.primary", background: "white"}}/></Box>
-                <Box sx={{ width: "582px", height: "95px", border: "2px solid black", borderRadius: "10px", ml: 1, mt: 1,   }}><ArrowCircleRightIcon sx={{ ml: 67, mt: 3, fontSize: 38}}/></Box>
-                <Box sx={{ width: "582px", height: "95px", border: "2px solid black", borderRadius: "10px", ml: 1, mt: 1,  }}><ArrowCircleRightIcon sx={{ ml: 67, mt: 3, fontSize: 38}}/></Box>
-                <Box sx={{ width: "582px", height: "95px", border: "2px solid black", borderRadius: "10px", ml: 1, mt: 1,  }}><ArrowCircleRightIcon sx={{ ml: 67, mt: 3, fontSize: 38}}/></Box>
-                <Box sx={{ width: "582px", height: "95px", border: "2px solid black", borderRadius: "10px", ml: 1, mt: 1,    }}><ArrowCircleRightIcon sx={{ ml: 67, mt: 3, fontSize: 38}}/></Box>
+                {/* <FontAwesomeIcon icon="fa-solid fa-bowl-food" /> */}
+                    <img src={food1} alt="test" width={"92px"} height={"90px"} display={"flex"} className="program" overflow={'hidden'} style={{borderRadius: '10px', }}/>
+                </Box>
+                <Box sx={{ width: "582px", height: "95px", border: "2px solid black", borderRadius: "10px", ml: 1, mt: 1,   }}>
+                    <img src={food2} alt="test" width={"110px"} height={"90px"} className="program" overflow={'hidden'}  style={{borderRadius: '20px', marginLeft: "px"}}/>
+                </Box>
+                <Box sx={{ width: "582px", height: "95px", border: "2px solid black", borderRadius: "10px", ml: 1, mt: 1,  }}>
+                    <img src={food3} alt="test" width={"110px"} height={"90px"} className="program" overflow={'hidden'}  style={{borderRadius: '20px', marginLeft: "px"}}/>
+                </Box>
+                <Box sx={{ width: "582px", height: "95px", border: "2px solid black", borderRadius: "10px", ml: 1, mt: 1,  }}>
+                    <img src={food4} alt="test" width={"120px"} height={"90px"} className="program" overflow={'hidden'}  style={{borderRadius: '20px', marginLeft: "px"}}/>
+                </Box>
+                <Box sx={{ width: "582px", height: "95px", border: "2px solid black", borderRadius: "10px", ml: 1, mt: 1,    }}>
+                    <img src={food5} alt="test" width={"120px"} height={"90px"} className="program" overflow={'hidden'}  style={{borderRadius: '20px', marginLeft: "px"}}/>
+                </Box>
             </Box>
             <Box sx={{ border: "1px solid black", height: "420px", width: "820px", borderRadius: "20px", mt: -52, ml: -36}}>
                 <Typography sx={{ position: "absolute", mt: -5, ml: 1, fontSize: 25, fontFamily: "serif" }}>
